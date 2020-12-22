8f22ea02e4f758adfb3750ee72e047e7207e6964
VladTzaru <bvanja.87@gmail.com>
2020-12-22 23:04:12 +0100

    Added login mutation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,7 +1,15 @@
 import { User } from '../entities/User';
 import argon2 from 'argon2';
 import { MyContext } from '../types';
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from 'type-graphql';
 
 @InputType()
 class UsernameAndPasswordInput {
@@ -12,13 +20,30 @@ class UsernameAndPasswordInput {
   password!: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
   async register(
     @Arg('options') options: UsernameAndPasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<User> {
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
@@ -26,5 +51,38 @@ export class UserResolver {
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernameAndPasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: 'username',
+            message: 'Invalid login credentials',
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: 'password',
+            message: 'Invalid login credentials',
+          },
+        ],
+      };
+    }
+
+    return {
+      user,
+    };
   }
 }
